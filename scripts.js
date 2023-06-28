7015f58676aad5449f0f46bebc70ce766458d138
Victor Rattes <137898547+victorrattesdev@users.noreply.github.com>
2023-06-28 20:52:49 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,53 @@
+function addTask() {
+
+    const taskTitle = document.querySelector("#task-title").value;
+
+    if(taskTitle) {
+
+        const template = document.querySelector(".template")
+
+        const newTask = template.cloneNode(true);
+
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function(){
+            removeTask(this);
+        });
+
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click",function(){
+            completeTask(this);
+        })
+
+
+
+
+        document.querySelector("#task-title").value="";
+    }
+}
+
+function removeTask(task) {
+    task.parentNode.remove(true);
+}
+
+function completeTask(task){
+    const taskToComplete = task.parentNode;
+
+    taskToComplete.classList.toggle("done");
+}
+
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    addTask();
+
+});
